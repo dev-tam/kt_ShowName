108678564870e1996ced13fd50e3b8ccaecfc004
TamjidMd <tamjid.mdh@gmail.com>
2017-12-04 21:03:33 +0600

    Added comments

--- app/src/main/java/com/tamtdk/tam/showname/MainActivity.kt
+++ app/src/main/java/com/tamtdk/tam/showname/MainActivity.kt
@@ -4,13 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { // MainActivity extends AppCompactActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var enteredText = enterNameEdt.text 
+        var enteredText = enterNameEdt.text // get data from text input field and store in enteredText
         showNameButton.setOnClickListener { resultId.text = enteredText }
     }
 }
